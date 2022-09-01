24bedbb12c302d9a760323a26117fe5ca564f7e6
BamBam-sys <lawalayobamidavid@gmail.com>
2022-09-01 03:25:24 +0200

    create faq section

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { BiMinus, BiPlus } from 'react-icons/bi';
 import './accordion.scss';
 
-const Accordion = () => {
+const Accordion = ({ label, content }) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+  const ref = useRef();
+
+  const active = accordionOpen ? 'active' : '';
+
   return (
-    <div className="accordion">
-      <div className="label">What is Ajoovest?</div>
-      <div className="content">Is Ajoovest a Savings App?</div>
+    <div className={`accordion ${active}`}>
+      <div className="label subTitle">{label}</div>
+      <div className="content bodyText" ref={ref}>
+        {content}
+      </div>
+      <div
+        className="icon"
+        onClick={() => {
+          setAccordionOpen(!accordionOpen);
+          accordionOpen
+            ? (ref.current.style.height = 0)
+            : (ref.current.style.height = ref.current.scrollHeight + 'px');
+        }}
+      >
+        {accordionOpen ? <BiMinus /> : <BiPlus />}
+      </div>
     </div>
   );
 };
 
 export default Accordion;
+
+// .style.height = panel.scrollHeight + 'px';
--- src/components/Faqs/Faqs.jsx
+++ src/components/Faqs/Faqs.jsx
@@ -5,11 +5,44 @@ import { ReactComponent as Dots } from '../../assets/images/blue-dots.svg';
 import './faqs.scss';
 
 const Faqs = () => {
+  const faqs = [
+    {
+      label: 'What is Ajoovest',
+      content: `Visit your preferred AjooVest/POS agent (or have them come to you) 
+      Give the agent cash and tell him purpose (interface allows user to save 
+        for Ajoo, Target, or specified reason)`,
+      id: 1,
+    },
+    {
+      label: 'Is Ajoovest a Savings App?',
+      content: `Visit your preferred AjooVest/POS agent (or have them come to you) 
+      Give the agent cash and tell him purpose (interface allows user to save 
+        for Ajoo, Target, or specified reason)`,
+      id: 2,
+    },
+    {
+      label: 'Is Ajoovest just for Ajoo Contributors?',
+      content: `Visit your preferred AjooVest/POS agent (or have them come to you) 
+      Give the agent cash and tell him purpose (interface allows user to save 
+        for Ajoo, Target, or specified reason)`,
+      id: 3,
+    },
+    {
+      label: 'What are the Benenfits of Ajoovest',
+      content: `Visit your preferred AjooVest/POS agent (or have them come to you) 
+      Give the agent cash and tell him purpose (interface allows user to save 
+        for Ajoo, Target, or specified reason)`,
+      id: 4,
+    },
+  ];
+
   return (
     <div className="faqs container">
       <div className="text">
         <h2 className="headerTwo">Frequently Asked Questions</h2>
-        <Accordion />
+        {faqs.map((faq) => (
+          <Accordion label={faq.label} content={faq.content} key={faq.id} />
+        ))}
       </div>
       <div className="img">
         <img src={faqImg} alt="trader" />
